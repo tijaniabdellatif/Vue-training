5957ab2ddeff5225749f38866c3965de37d0bef4
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2021-08-21 21:48:47 +0200

    initiale

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <todo></todo>
+
+  </div>
+</template>
+
+
+<script>
+
+import Todo from './components/Todo'
+
+export default {
+  
+  name:'App',
+  components:{
+    Todo
+  }
+
+}
+
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,53 @@
+<template>
+
+<secton class="todoapp">
+
+<header>
+    <h1 class="">
+        Todos
+    </h1>
+
+    <input type="text" class="new-todo" placeholder="Add a todo" />
+</header>
+
+  <div class="main">
+
+       <ul class="todo-list">
+          <li class="todo">
+                  <div class="view">
+                      <label> Nom de la tache </label>
+                  </div>
+          </li>
+      </ul>
+
+  </div>
+</secton>
+    
+</template>
+
+
+<script>
+
+export default {
+
+
+
+    data(){
+
+        return {
+           todos : [{
+
+               name: 'Task Test',
+               completed : false
+
+           }]
+        }
+    }
+}
+
+</script>
+
+
+<style src="./todos.css">
+
+</style>
